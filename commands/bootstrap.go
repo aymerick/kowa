1275b52f64c8ffd302c264e8c14ad5e3f3361249
Aymerick <aymerick@jehanne.org>
2014-11-24 11:17:31 +0100

    Adds fixtures

--- commands/bootstrap.go
+++ commands/bootstrap.go
@@ -19,23 +19,53 @@ var bootstrapCmd = &cobra.Command{
 func bootstrap(cmd *cobra.Command, args []string) {
 	// @todo Check that we are NOT in production
 
-	// Insert user
-	user := models.User{
+	// Insert users
+	userJeanClaude := models.User{
 		Id:        bson.NewObjectId(),
 		FirstName: "Jean-Claude",
 		LastName:  "Trucmush",
 		CreatedAt: time.Now(),
 	}
-	models.UsersCol().Insert(&user)
+	models.UsersCol().Insert(&userJeanClaude)
 
-	// Insert site
-	site := models.Site{
+	userHenry := models.User{
+		Id:        bson.NewObjectId(),
+		FirstName: "Henry",
+		LastName:  "Kanan",
+		CreatedAt: time.Now(),
+	}
+	models.UsersCol().Insert(&userHenry)
+
+	// Insert sites
+	var site models.Site
+
+	site = models.Site{
 		Id:          bson.NewObjectId(),
-		UserId:      user.Id,
+		UserId:      userJeanClaude.Id,
 		CreatedAt:   time.Now(),
 		Name:        "My site",
 		Tagline:     "So powerfull !",
 		Description: "You will be astonished by what my site is about",
+	}
+	models.SitesCol().Insert(&site)
+
+	site = models.Site{
+		Id:          bson.NewObjectId(),
+		UserId:      userJeanClaude.Id,
+		CreatedAt:   time.Now(),
+		Name:        "My second site",
+		Tagline:     "Very interesting",
+		Description: "Our projects are so importants, please help us",
+	}
+	models.SitesCol().Insert(&site)
+
+	site = models.Site{
+		Id:          bson.NewObjectId(),
+		UserId:      userHenry.Id,
+		CreatedAt:   time.Now(),
+		Name:        "Ultimate petanque",
+		Tagline:     "La petanque comme vous ne l'avez jamais vu",
+		Description: "C'est vraiment le sport du futur. Messieurs, preparez vos boules !",
 	}
 	models.SitesCol().Insert(&site)
 
