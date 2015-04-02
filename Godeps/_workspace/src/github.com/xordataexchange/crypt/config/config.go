ec297315271500af4d39d5249cd776eb8176d042
Aymerick <aymerick@jehanne.org>
2015-04-02 15:03:27 +0200

    Regenerated Godeps/ entirely

--- Godeps/_workspace/src/github.com/xordataexchange/crypt/config/config.go
+++ Godeps/_workspace/src/github.com/xordataexchange/crypt/config/config.go
@@ -11,19 +11,39 @@ import (
 	"github.com/xordataexchange/crypt/encoding/secconf"
 )
 
-// A ConfigManager retrieves and decrypts configuration from a key/value store.
-type ConfigManager interface {
-	Get(key string) ([]byte, error)
-	Watch(key string, stop chan bool) <-chan *Response
+type KVPair struct {
+	backend.KVPair
 }
+
+type KVPairs []*KVPair
 
 type configManager struct {
 	keystore []byte
 	store    backend.Store
 }
 
+// A ConfigManager retrieves and decrypts configuration from a key/value store.
+type ConfigManager interface {
+	Get(key string) ([]byte, error)
+	List(key string) (KVPairs, error)
+	Set(key string, value []byte) error
+	Watch(key string, stop chan bool) <-chan *Response
+}
+
 type standardConfigManager struct {
 	store backend.Store
+}
+
+func NewStandardConfigManager(client backend.Store) (ConfigManager, error) {
+	return standardConfigManager{client}, nil
+}
+
+func NewConfigManager(client backend.Store, keystore io.Reader) (ConfigManager, error) {
+	bytes, err := ioutil.ReadAll(keystore)
+	if err != nil {
+		return nil, err
+	}
+	return configManager{bytes, client}, nil
 }
 
 // NewStandardEtcdConfigManager returns a new ConfigManager backed by etcd.
@@ -32,7 +52,8 @@ func NewStandardEtcdConfigManager(machines []string) (ConfigManager, error) {
 	if err != nil {
 		return nil, err
 	}
-	return standardConfigManager{store}, nil
+
+	return NewStandardConfigManager(store)
 }
 
 // NewStandardConsulConfigManager returns a new ConfigManager backed by consul.
@@ -41,7 +62,7 @@ func NewStandardConsulConfigManager(machines []string) (ConfigManager, error) {
 	if err != nil {
 		return nil, err
 	}
-	return standardConfigManager{store}, nil
+	return NewStandardConfigManager(store)
 }
 
 // NewEtcdConfigManager returns a new ConfigManager backed by etcd.
@@ -51,24 +72,17 @@ func NewEtcdConfigManager(machines []string, keystore io.Reader) (ConfigManager,
 	if err != nil {
 		return nil, err
 	}
-	bytes, err := ioutil.ReadAll(keystore)
-	if err != nil {
-		return nil, err
-	}
-	return configManager{bytes, store}, nil
+	return NewConfigManager(store, keystore)
 }
 
 // NewConsulConfigManager returns a new ConfigManager backed by consul.
+// Data will be encrypted.
 func NewConsulConfigManager(machines []string, keystore io.Reader) (ConfigManager, error) {
 	store, err := consul.New(machines)
 	if err != nil {
 		return nil, err
 	}
-	bytes, err := ioutil.ReadAll(keystore)
-	if err != nil {
-		return nil, err
-	}
-	return configManager{bytes, store}, nil
+	return NewConfigManager(store, keystore)
 }
 
 // Get retrieves and decodes a secconf value stored at key.
@@ -89,6 +103,55 @@ func (c standardConfigManager) Get(key string) ([]byte, error) {
 		return nil, err
 	}
 	return value, err
+}
+
+// List retrieves and decodes all secconf value stored under key.
+func (c configManager) List(key string) (KVPairs, error) {
+	list, err := c.store.List(key)
+	retList := make(KVPairs, len(list))
+	if err != nil {
+		return nil, err
+	}
+	for i, kv := range list {
+		retList[i].Key = kv.Key
+		retList[i].Value, err = secconf.Decode(kv.Value, bytes.NewBuffer(c.keystore))
+		if err != nil {
+			return nil, err
+		}
+	}
+	return retList, nil
+}
+
+// List retrieves all values under key.
+// convenience function, no additional value provided over
+// `etcdctl`
+func (c standardConfigManager) List(key string) (KVPairs, error) {
+	list, err := c.store.List(key)
+	retList := make(KVPairs, len(list))
+	if err != nil {
+		return nil, err
+	}
+	for i, kv := range list {
+		retList[i].Key = kv.Key
+		retList[i].Value = kv.Value
+	}
+	return retList, err
+}
+
+// Set will put a key/value into the data store
+// and encode it with secconf
+func (c configManager) Set(key string, value []byte) error {
+	encodedValue, err := secconf.Encode(value, bytes.NewBuffer(c.keystore))
+	if err == nil {
+		err = c.store.Set(key, encodedValue)
+	}
+	return err
+}
+
+// Set will put a key/value into the data store
+func (c standardConfigManager) Set(key string, value []byte) error {
+	err := c.store.Set(key, value)
+	return err
 }
 
 type Response struct {
